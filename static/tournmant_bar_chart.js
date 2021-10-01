715df1ab492e4e00ea16b7009d4464fc3db35eab
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-09-30 22:14:19 -0700

    added basic chart

--- static/tournmant_bar_chart.js
+++ static/tournmant_bar_chart.js
@@ -1,13 +1,44 @@
 sample = "static/top_women_chess_players_aug_2020.json"
 var gdata
+let name_list = []
+let standard_rating_list = []
 // Create Bar Chart
 // function barChart(x){
 d3.json(sample).then(function(data) {
     console.log(data);
     gdata = data
-    g = gdata[0]
-    // var attempt = g.filter(m => m.id == x)[0]
-    var name_id = g.filter(m => m.Name == x)[0];
-    console.log(name_id)
+
+    for (i =0; i < data.length; i++){
+        name_list.push(data[i].Fideid)
+        standard_rating_list.push(data[i].Standard_Rating)
+        // var names_id = data[i].Fideid   
+    }
+    console.log(name_list)
+    console.log(standard_rating_list)
+    // g = gdata[0]["Fideid"]
+    // console.log(g)
+        // Slice the first 10 objects for plotting
+        slicedRating = standard_rating_list.slice(0, 15);
+        slicedNames = name_list.slice(0,15);
+        slicedNames = slicedNames.map(L => "ID " + L)
+
+
+    // // Trace1 for the OTU Data
+    let trace1 = {
+        x: slicedNames,
+        y: slicedRating,
+        text: slicedNames,
+        name: "Tournament Bar Chart",
+        type: "bar"
+        // orientation: "h"
+        };
+  
+  let traceData = [trace1];
+  
+  // Note that we use `traceData` here, not `data`
+  Plotly.newPlot("bar", traceData);
+
+
+    
   });
 // }
