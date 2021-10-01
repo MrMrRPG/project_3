4d9c89e483e24c82385e5188e9bcdd7954984a7a
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-09-30 21:22:25 -0700

    change tournament link

--- static/tournmant_bar_chart.js
+++ static/tournmant_bar_chart.js
@@ -1,6 +1,8 @@
-sample = "static/games.json"
+sample = "static/top_women_chess_players_aug_2020.json"
 
+// Create Bar Chart
+function barChart(x){
 d3.json(sample).then(function(data) {
     console.log(data);
   });
-  
+}
