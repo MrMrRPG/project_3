815002f3be6bc31a98703e14fbd969dd5b6dc5b0
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-09-30 21:38:52 -0700

    modified file

--- static/tournmant_bar_chart.js
+++ static/tournmant_bar_chart.js
@@ -1,8 +1,13 @@
 sample = "static/top_women_chess_players_aug_2020.json"
-
+var gdata
 // Create Bar Chart
-function barChart(x){
+// function barChart(x){
 d3.json(sample).then(function(data) {
     console.log(data);
+    gdata = data
+    g = gdata[0]
+    // var attempt = g.filter(m => m.id == x)[0]
+    var name_id = g.filter(m => m.Name == x)[0];
+    console.log(name_id)
   });
-}
+// }
