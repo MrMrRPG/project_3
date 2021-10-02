67ce73ba8cf0b9abe610096987a58a9f8890479e
MrMrRPG <nng5998@gmail.com>
2021-10-02 10:48:39 -0700

    added some stuff on app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from collections import Iterable
+
 # 1. import Flask
 from flask import Flask, jsonify
 
@@ -9,14 +11,21 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
-    return "Welcome to my the Pink Pandas' Chess Directory!"
+    return (
+        f"Welcome to my the Pink Pandas' Chess Directory!<br/>"
+        f"Use the following url extensions to get to the specific APIs you need:)<br/>"
+        f"------------------------------------------------------------------------<br/>"
+        f"/about<br/>"
+        )
 
 
 # 4. Define what to do when a user hits the /about route
 @app.route("/about")
 def about():
     print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+    return (
+        f"Welcome to my 'About' page!<br/>"
+    )
 
 
 if __name__ == "__main__":
