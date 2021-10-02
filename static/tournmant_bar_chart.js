2f77d88893423b580f17113390b52eaae49888f4
Meagan Murray-Bruce <meaganmb_11@Meagans-MacBook-Pro.local>
2021-10-02 11:30:21 -0700

    added png and code for tournament scatter

--- static/tournmant_bar_chart.js
+++ static/tournmant_bar_chart.js
@@ -1,47 +1,46 @@
 sample = "static/top_women_chess_players_aug_2020.json"
 // var gdata
-let name_list = []
-let id_list = []
-let standard_rating_list = []
-// Create Bar Chart
-// function barChart(x){
-d3.json(sample).then(function(data) {
-    console.log(data);
-    // gdata = data
-
-    for (i =0; i < data.length; i++){
-        name_list.push(data[i].Name)
-        id_list.push(data[i].Fideid)
-        standard_rating_list.push(data[i].Standard_Rating)
-        // var names_id = data[i].Fideid   
-    }
-    console.log(name_list)
-    console.log(standard_rating_list)
-    // g = gdata[0]["Fideid"]
-    // console.log(g)
-        // Slice the first 10 objects for plotting
-        slicedName = name_list.slice(0,15);
-        slicedRating = standard_rating_list.slice(0, 15);
-        slicedID = id_list.slice(0,15);
-        slicedID = slicedID.map(L => "ID " + L)
-
-
-    // // Trace1 for the OTU Data
-    let trace1 = {
-        x: slicedID,
-        y: slicedRating,
-        text: slicedName,
-        name: "Tournament Bar Chart",
-        type: "bar"
-        // orientation: "h"
-        };
+// Attempting to use google charts
+google.charts.load('current',{packages:['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+name_list = []
+function drawChart() {
+    d3.json(sample).then(function(dataValues) {
+        console.log(dataValues); 
+            var data = new google.visualization.DataTable();  
   
-  let traceData = [trace1];
-  
-  // Note that we use `traceData` here, not `data`
-  Plotly.newPlot("bar", traceData);
-
+                data.addColumn('number', 'money');  
+                data.addColumn('number', 'Ratings');
+                data.addColumn({type: 'string', role: 'tooltip'});
 
     
-  });
-// }
+            for (var i = 0; i < dataValues.length; i++)
+            
+            {  
+                try{
+                data.addRow([parseInt(dataValues[i].Rapid_rating), parseInt(dataValues[i].Standard_Rating), dataValues[i].Name] ); 
+                } catch(error){console.error(error)}
+            }
+            // data.addColumn('number', name_list); 
+            // console.log(data)
+
+  var options = {
+    tooltip: {isHtml: true},
+    title: 'Tournament Ratings',
+    // hAxis: {title: 'Rapid Rating', minValue: 1800, maxValue: 3000},
+    hAxis: {title: 'Rapid Rating',
+            viewWindow: {
+                min: 1210,
+                max: 2700
+            }},
+    vAxis: {title: 'Standard Rating', minValue: 1800, maxValue: 2700},
+    legend: 'none',
+    // legend: name_list,
+    pointSize: 0.2,
+  };
+
+  var chart = new google.visualization.ScatterChart(document.getElementById('scatter'));
+
+  chart.draw(data, options);
+});
+}
